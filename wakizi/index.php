63b22762feb5ccf3ec4b7a6dece7166da74bbc86
Jan Sorgalla <jsorgalla@gmail.com>
2015-03-27 14:21:23 +0100

    Update post template headline

--- wakizi/index.php
+++ wakizi/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
             <div class="content" role="main">
-                <h1 class="entry-title">Aktuelles</h1>
+                <h1 class="entry-title">Meldungen</h1>
                 <?php while (have_posts()): the_post(); ?>
                     <?php get_template_part('parts/content', get_post_format()); ?>
                 <?php endwhile; ?>
